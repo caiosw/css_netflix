ff0d1210203b531ac76308f7e0958776eeb90bb1
Caio Werneck <caiosw@gmail.com>
2020-10-17 05:26:53 -0300

    wip

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ function renderCarouselThumbnails () {
   for (let i = 0; i < 10; i++) {
     var img = $('<img>').addClass('box-movie');
     img.attr('src', `img/thumb${i + 1}.jpg`);
+    img.attr('data-movie-info', `id_${i + 1}`);
+    img.attr('data-custom-open', 'modal-info');
   
     var item = $('<div>').addClass('item');
     item.append(img);
@@ -9,6 +11,29 @@ function renderCarouselThumbnails () {
     $('#home_carousel').append(item);
   }
 }
+
+// mousedown isn't the best option, but it managed to avoid opening the wrong movie 
+// acidentally due the drag and drop
+$('body').on('mousedown', '[data-movie-info]', function (e) {
+  var id = $(e.target).data('movie-info');
+  $('#modal_title').text(movies_data[id].name);
+  $('#modal_description').text(movies_data[id].description);
+  $('#modal_image').attr('src', movies_data[id].img);
+  $('#modal_watch_btn').attr('data-movie-watch', id);
+});
+
+$('body').on('click', '[data-movie-watch]', function (e) {
+  window.scrollTo(0, 0)
+  MicroModal.close('modal-info'); 
+
+  var id = $(e.target).data('movie-watch');
+
+  $('section#home').addClass('d-none');
+  $('section#theater').removeClass('d-none');
+  $('#video_player').attr('src', `https://www.youtube.com/embed/${movies_data[id].video_id}`);
+  
+  $('#movie_title').text(movies_data[id].name);
+});
 
 renderCarouselThumbnails();
 
@@ -28,3 +53,16 @@ $('.owl-carousel').owlCarousel({
     }
   }
 })
+
+MicroModal.init({
+  onShow: (modal, trigger) => console.info(`${modal.id} is shown`, modal), // [1]
+  onClose: modal => console.info(`${modal.id} is hidden`), // [2]
+  openTrigger: 'data-custom-open', // [3]
+  closeTrigger: 'data-custom-close', // [4]
+  openClass: 'is-open', // [5]
+  disableScroll: true, // [6]
+  disableFocus: false, // [7]
+  awaitOpenAnimation: false, // [8]
+  awaitCloseAnimation: false, // [9]
+  debugMode: true // [10]
+});
---
+++ js/movies_data.js
@@ -0,0 +1,88 @@
+var movies_data = {
+  id_0: {
+    name: 'ENOLA HOLMES',
+    description: 'While searching for her missing mother, intrepid teen Enola Holmes' +
+      'uses her sleuthing skills to outsmart big brother Sherlock and help a runaway lord.',
+    img: 'img/thumb0.jpg',
+    video_id: 'O2qGj29YA2s'
+  },
+  id_1: {
+    name: '2067',
+    description: 'A lowly utility worker is called to the future by a mysterious radio signal, ' +
+      'he must leave his dying wife to embark on a journey that will force him to face his deepest ' +
+      'fears in an attempt to change the fabric of reality and save humankind from its greatest ' +
+      'environmental crisis yet.',
+    img: 'img/thumb1.jpg',
+    video_id: 'Fk3NhzNqAuY'
+  },
+  id_2: {
+    name: 'AMERICAN PIE PRESENTS: GIRLS\' RULES',
+    description: 'It\'s Senior year at East Great Falls. Annie, Kayla, Michelle, and Stephanie decide ' +
+      'to harness their girl power and band together to get what they want their last year of high school.',
+    img: 'img/thumb2.jpg',
+    video_id: 'KdP-0MJ6oEE'
+  },
+  id_3: {
+    name: 'HARD KILL',
+    description: 'The work of billionaire tech CEO Donovan Chalmers is so valuable that he hires ' +
+      'mercenaries to protect it, and a terrorist group kidnaps his daughter just to get it.',
+    img: 'img/thumb3.jpg',
+    video_id: 'oP99YZiEYtk'
+  },
+  id_4: {
+    name: 'MULAN',
+    description: 'When the Emperor of China issues a decree that one man per family must serve in the ' +
+      'Imperial Chinese Army to defend the country from Huns, Hua Mulan, the eldest daughter of an ' +
+      'honored warrior, steps in to take the place of her ailing father. She is spirited, determined ' +
+      'and quick on her feet. Disguised as a man by the name of Hua Jun, she is tested every step of ' +
+      'the way and must harness her innermost strength and embrace her true potential.',
+    img: 'img/thumb4.jpg',
+    video_id: '01ON04GCwKs'
+  },
+  id_5: {
+    name: 'MONEY PLANE',
+    description: 'A professional thief with $40 million in debt and his family\'s life on the line must ' +
+      'commit one final heist - rob a futuristic airborne casino filled with the world\'s most dangerous ' +
+      'criminals.',
+    img: 'img/thumb5.jpg',
+    video_id: 'aETz_dRDEys'
+  },
+  id_6: {
+    name: 'BECKMAN',
+    description: 'A contract killer, becomes the reverend of a LA church, until a cult leader and his ' +
+      'minions kidnap his daughter. Blinded by vengeance, he cuts a bloody path across the city. The only ' +
+      'thing that can stop him is his newfound faith.',
+    img: 'img/thumb6.jpg',
+    video_id: 'Ed4u4g0xP4s'
+  },
+  id_7: {
+    name: 'ROGUE',
+    description: 'Battle-hardened O’Hara leads a lively mercenary team of soldiers on a daring mission: ' +
+      'rescue hostages from their captors in remote Africa. But as the mission goes awry and the team is ' +
+      'stranded, O’Hara’s squad must face a bloody, brutal encounter with a gang of rebels.',
+    img: 'img/thumb7.jpg',
+    video_id: 'qRQ8-Iq4RqA'
+  },
+  id_8: {
+    name: 'SANTANA',
+    description: 'Two brothers — one a narcotics agent and the other a general — finally discover the ' +
+      'identity of the drug lord who murdered their parents decades ago. They may kill each other before' +
+      'capturing the bad guys.',
+    img: 'img/thumb8.jpg',
+    video_id: 'QN5P8vISrHM'
+  },
+  id_9: {
+    name: 'AVA',
+    description: 'A black ops assassin is forced to fight for her own survival after a job goes ' +
+      'dangerously wrong.',
+    img: 'img/thumb9.jpg',
+    video_id: 'p1-A8UITU18'
+  },
+  id_10: {
+    name: 'PENINSULA',
+    description: 'A soldier and his team battle hordes of post-apocalyptic zombies in the wastelands ' +
+      'of the Korean Peninsula.',
+    img: 'img/thumb10.jpg',
+    video_id: 'xhNwhL58c9E'
+  }
+}
